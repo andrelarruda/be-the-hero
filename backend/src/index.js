7a2f6ff06456d31671c8fe46b2a0665917213c33
Andre Arruda <andre.luizarruda@ufrpe.br>
2020-03-30 12:06:00 -0300

    adicionando validaçao com celebrate.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.urlencoded( { extended: false }));
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+//Validação: vídeo "Funcionalidades Avançadas" (4:10 - 21:15)
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -8,15 +10,51 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create);
+routes.post('/sessions', celebrate({
+   [Segments.BODY]: Joi.object().keys({
+      id: Joi.string().length(8).required(),
+   })
+}), SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+   [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required().min(10).max(11),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2),
+   })
+}), OngController.create);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/incidents', celebrate({
+   [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+   })
+}), IncidentController.index);
+routes.post('/incidents', celebrate({
+   [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+   }).unknown(),
+   [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+   })
+}), IncidentController.create);
+routes.delete('/incidents/:id', celebrate({
+   [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+   }).unknown(),
+   [Segments.BODY]: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      value: Joi.number().required(),
+   })
+}), IncidentController.delete);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+   [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+   }).unknown(), // Usamos o 'unknown' pois não sabemos quais são tudo o que vai ser enviado no header. Mesmo que enviemos apenas um parâmetro pelo header, não sabemos quais são os obrigatórios.
+}), ProfileController.index);
 
 module.exports = routes;
